8a5b8fc37c007b7152334d744f8be2028913f689
jihanzhang <dreck.zhang@gmail.com>
2019-01-23 22:19:17 -0500

    showEdit

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -62,6 +62,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/delete":
 					deleteBook(request, response);
             break;
+				case "/edit":
+					showEditForm(request, response);
+			break;
 				default:
 				    listBooks(request, response);
             break;
@@ -71,6 +74,7 @@ public class ControllerServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
+
 
 	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response)
 			throws ClassNotFoundException, SQLException, ServletException, IOException {
@@ -115,7 +119,15 @@ public class ControllerServlet extends HttpServlet {
 		
 		response.sendRedirect("list");
 	}
-
+	
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Book existingBook = bookDAO.getBook(id);
+		request.setAttribute("book", existingBook);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		dispatcher.forward(request, response);
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
