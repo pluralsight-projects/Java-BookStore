e1dd72b1186e206fad50d2ee8d025201f2c66348
jihanzhang <dreck.zhang@gmail.com>
2019-01-24 20:29:36 -0500

    Cart1

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
