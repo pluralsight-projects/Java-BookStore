13f4bdd43280a04441d9531d76995c30a7a699ac
Majid Khan <maji905@aol.com>
2019-08-27 18:51:44 +0200

    Completed the module

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -110,7 +110,39 @@ public class BookDAO {
 			System.err.println("some problem"+e.getMessage());
 					// TODO Auto-generated catch block
 			e.printStackTrace();
+			}}
+    	public void updateBook(Book book) {
+    		
+    		String SQL="UPDATE book  SET title = ?,author = ?,price = ? " + "WHERE id = ? ";
+    		
+    		try {
+				PreparedStatement statement = jdbcConnection.prepareStatement(SQL);
+				
+				statement.setString(1, book.getTitle());
+				statement.setString(2, book.getAuthor());
+				statement.setFloat(3, book.getPrice());
+				statement.setInt(4, book.getId());
+				
+				statement.executeUpdate();
+				statement.close();
+			} catch (SQLException e) {
+				System.err.println("Some thing Wrong"+e.getMessage());
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+    		
+
+
+
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    	
     			
     }
 }
